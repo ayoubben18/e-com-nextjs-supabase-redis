f5f518b2d010e1197055432668a828e85a863758
ayoubben18 <ayoubbensalah2004@gmail.com>
2024-06-22 13:39:21 +0100

    refactor: Remove unused code and optimize page load performance

--- mappers/checkoutMapper.ts
+++ mappers/checkoutMapper.ts
@@ -1,24 +1,23 @@
-import { CheckoutItemType } from "@/types/DtoTypes";
-import { Order, Product } from "@/types/tablesTypes";
+// import { CheckoutItemType } from "@/types/DtoTypes";
+// import { Order, Product } from "@/types/tablesTypes";
 
-export function mapCheckoutMapToCheckoutItemArray(
-  checkoutmap: Map<Order, Product>,
-): CheckoutItemType[] {
-  const checkoutItems: CheckoutItemType[] = [];
+// export function mapCheckoutMapToCheckoutItemArray(
+//   checkoutmap: Map<Order, Product>,
+// ): CheckoutItemType[] {
+//   const checkoutItems: CheckoutItemType[] = [];
 
-  checkoutmap.forEach((product, order) => {
-    const checkoutItem: CheckoutItemType = {
-      id: order.id,
-      name: product.name,
-      image: null,
-      price: order.price!,
-      quantity: order.quantity,
-      color: order.color,
-      size: order.size,
-    };
+//   checkoutmap.forEach((product, order) => {
+//     const checkoutItem: CheckoutItemType = {
+//       id: order.id,
+//       image: null,
+//       price: order.price!,
+//       quantity: order.quantity,
+//       color: order.color,
+//       size: order.size,
+//     };
 
-    checkoutItems.push(checkoutItem);
-  });
+//     checkoutItems.push(checkoutItem);
+//   });
 
-  return checkoutItems;
-}
+//   return checkoutItems;
+// }
