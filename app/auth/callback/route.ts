b750e4818828dd570051e615d431736af1318a22
ayoubben18 <ayoubbensalah2004@gmail.com>
2024-06-25 13:39:53 +0100

    refactor: Update authentication redirect URLs to use environment-specific values

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
   // if "next" is in param, use it as the redirect URL
-  // const next = searchParams.get("next") ?? "/";
+  const next = searchParams.get("next") ?? "/";
 
   if (code) {
     const cookieStore = cookies();
@@ -29,12 +29,10 @@ export async function GET(request: Request) {
     );
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
-      return NextResponse.redirect(`${process.env.NEXT_PUBLIC_URL!}`);
+      return NextResponse.redirect(`${origin}${next}`);
     }
   }
 
   // return the user to an error page with instructions
-  return NextResponse.redirect(
-    `${process.env.NEXT_PUBLIC_URL!}/auth/auth-code-error`,
-  );
+  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
 }
